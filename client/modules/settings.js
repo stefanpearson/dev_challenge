971861bdf00ae0bd7983e3adfeca30bd6c58f8ec
Stefan Pearson <stefanpearson@hotmail.co.uk>
2013-05-03 15:12:06 +0100

    renamed ribot.js to app.'s, changed api url to temporary live api for testing

--- client/modules/settings.js
+++ client/modules/settings.js
@@ -1,9 +1,11 @@
 define([], function () {
 
-    var live_server = false;
+    var live_api_url = '//theribots.nodejitsu.com/api/',
+        alt_live_api_url = '//riapi.eu01.aws.af.cm/api/',
+        local_api_url = '//'+window.location.hostname+':25708/api/';
 
     return {
-        riAPI_url: (live_server ? '//theribots.nodejitsu.com/api/' : '//'+window.location.hostname+':25708/api/'),
+        riAPI_url: alt_live_api_url,
         overflow_scrolling: false
     };
 });
